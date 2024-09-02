0bad03535e4b8404e54446e048b6597b02fae3cf
Clément <six@localhost.localdomain>
2024-09-02 15:32:45 -0400

    Tidy source code.

--- source/code/snippets/partiallyFilledArray.cs
+++ source/code/snippets/partiallyFilledArray.cs
@@ -2,41 +2,41 @@ using System;
 
 public class Program
 {
-    public static void Main(string[] args)
+  public static void Main(string[] args)
+  {
+    // We decide that the maximum number of input is 10.
+    const int MAXSIZE = 10;
+
+    int[] inputs = new int[MAXSIZE];
+
+    // The following variable will contain the number of input actually given.
+    int numberOfInputs = 0;
+
+    // The following variable will hold the user input.
+    string uInput;
+
+    do
     {
-        // We decide that the maximum number of input is 10.
-        const int MAXSIZE = 10;
-
-        int[] inputs = new int[MAXSIZE];
-
-        // The following variable will contain the number of input actually given.
-        int numberOfInputs = 0;
-
-        // The following variable will hold the user input.
-        string uInput;
-
-        do
-        {
-            Console.WriteLine(
-              "What is your input #" + (numberOfInputs + 1) + "? Enter \"done\" when you are done."
-            );
-            uInput = Console.ReadLine();
-            if (uInput != "done")
-            {
-                inputs[numberOfInputs] = int.Parse(uInput);
-                numberOfInputs++; // We increment the number of items in the list.
-            }
-            if (numberOfInputs == MAXSIZE)
-            {
-                Console.WriteLine(
-                  "You have reached the maximum number of inputs."
-                );
-            }
-        } while (uInput != "done" && numberOfInputs < MAXSIZE);
-        /* 
-         * When the user enters "done", or if the user reached the maximum number of inputs, we exit this loop.
-         */
-
-    }
+      Console.WriteLine(
+        "What is your input #"
+          + (numberOfInputs + 1)
+          + "? Enter \"done\" when you are done."
+      );
+      uInput = Console.ReadLine();
+      if (uInput != "done")
+      {
+        inputs[numberOfInputs] = int.Parse(uInput);
+        numberOfInputs++; // We increment the number of items in the list.
+      }
+      if (numberOfInputs == MAXSIZE)
+      {
+        Console.WriteLine(
+          "You have reached the maximum number of inputs."
+        );
+      }
+    } while (uInput != "done" && numberOfInputs < MAXSIZE);
+    /*
+     * When the user enters "done", or if the user reached the maximum number of inputs, we exit this loop.
+     */
+  }
 }
-
