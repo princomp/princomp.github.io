839e2b55533f7150358d47b68c81bad835328bbc
Clément <six@localhost.localdomain>
2024-10-21 21:55:09 -0400

    Prettified code.

--- source/code/snippets/setToRandom.cs
+++ source/code/snippets/setToRandom.cs
@@ -1,16 +1,17 @@
 using System;
+
 class Program
 {
-    static void Main()
-    {
-        int a;
-        SetToRandom(out a);
-        Console.WriteLine(a);
-    }
-    static void SetToRandom(out int a)
-    {
-        Random gen = new Random();
-        a = gen.Next(10);
-    }
-}
+  static void Main()
+  {
+    int a;
+    SetToRandom(out a);
+    Console.WriteLine(a);
+  }
 
+  static void SetToRandom(out int a)
+  {
+    Random gen = new Random();
+    a = gen.Next(10);
+  }
+}
