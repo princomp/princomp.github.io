d3db05a361b0233005996f641695e2ffb24dd3bf
Clément <six@localhost.localdomain>
2024-10-30 23:52:40 -0400

    Adding exercises.

---
+++ source/code/projects/ListFoldersFileRecursively/ListFoldersFileRecursively/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+  static void Main()
+  {
+        string root = AppDomain.CurrentDomain.BaseDirectory;
+        Console.Write(root);
+        DirSearch_ex3("c:\\aaa");
+    }
+    static void DirSearch_ex3(string sDir)
+    {
+        //Console.WriteLine("DirSearch..(" + sDir + ")");
+        try
+        {
+            Console.WriteLine(sDir);
+
+            foreach (string f in Directory.GetFiles(sDir))
+            {
+                Console.WriteLine(f);
+            }
+
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                DirSearch_ex3(d);
+            }
+        }
+        catch (System.Exception excpt)
+        {
+            Console.WriteLine(excpt.Message);
+        }
+    }
+}
