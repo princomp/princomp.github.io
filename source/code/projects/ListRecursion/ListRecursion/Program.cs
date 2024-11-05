3b109549bc67d19adea8ecfee1e405320f8f9e91
Clément <six@localhost.localdomain>
2024-11-04 23:07:56 -0500

    Fixing exercises, again.

---
+++ source/code/projects/ListRecursion/ListRecursion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+        void CountDown(int n)
+        {
+            if(n == 0){ Console.WriteLine($"{n}: Blast off!"); }
+            else 
+        }
+    }
+}
