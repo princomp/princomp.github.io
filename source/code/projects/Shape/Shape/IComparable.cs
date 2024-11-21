6c21fc90edff4e5f67f9c91e1e9d34471048d713
caubert <caubert@augusta.edu>
2024-11-21 13:34:13 -0500

    Added clarification on the generic interface.

--- source/code/projects/Shape/Shape/IComparable.cs
+++ source/code/projects/Shape/Shape/IComparable.cs
@@ -1,8 +1,18 @@
+/* 
+ * This class uses the generic interface,
+ * https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/generic-interfaces
+ * The generic T must be instantiated.
+ * This code is actually very close to the one used by C#, 
+ * cf.
+ * https://learn.microsoft.com/en-us/dotnet/api/system.icomparable-1.compareto?view=net-9.0
+ */
 public interface IComparable<T>
 {
   int CompareTo(T arg);
-  /* Compares this object with the specified object for order.
+  /* Compares the calling object with
+   * the object passed as an argument.
    * Returns a negative integer, zero, or a positive
-   * integer as this object is less than, equal to, or greater
+   * integer as the calling object is less than,
+   * equal to, or greater
    * than the specified object.*/
 }
