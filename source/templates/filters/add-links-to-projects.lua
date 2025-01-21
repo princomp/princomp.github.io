61450363436eccb4458ff43e5a5a7b7668703dca
Clément <Clment@domicile>
2025-01-21 13:49:06 -0500

    Improving download button filter.

--- source/templates/filters/add-links-to-projects.lua
+++ source/templates/filters/add-links-to-projects.lua
@@ -6,15 +6,16 @@ return {
     {
         CodeBlock = function(el)
             if el.attributes.download ~= nil then
-                link = el.attributes.download
-                el.attributes.download=nil
+                link = el.attributes.download -- We save the link before removing this attribute from the element.
+                el.attributes.download=nil    -- removing the download attribute from the element altogether.
                 return {
                     el,
                     pandoc.Link(
                         pandoc.Emph("(Download this code)"),
                         link,
-                        "Download a project containing this snippet.",
-                        {class = "download-button"}
+                        "Download a project containing this snippet."
+                        -- ,  {class = "download-button"} -- unfortunately, Quartz (Github Flavored Markdown // Obsidian Flavored Markdown)
+                        -- does not seem to support classes.
                     )
                 }
             end
