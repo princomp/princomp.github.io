2723347cfde8a31dfc5f714c9e2c6ad32f48e5bf
Clément <Clment@domicile>
2025-02-10 17:42:28 -0500

    Adding program for Bookmarker project.

---
+++ source/code/projects/Bookmarker/Bookmarker/Bookmarker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Bookmarker
+{
+    public string Title { get; set; }
+    private int tPages;
+    public string TPages
+    {
+        set
+        {
+            int input = int.Parse(value);
+            if (input <= 0) throw new ArgumentException(
+         "The total number of pages cannot be negative."
+       );
+            else tPages = input;
+        }
+        get { return tPages.ToString(); }
+    }
+    private int cPages;
+    public string CPages
+    {
+        set
+        {
+            int input = int.Parse(value);
+            if (input < 0) throw new ArgumentException(
+          "You cannot have read a negative number of pages!"
+        );
+            else if (input > tPages)
+            {
+                throw new ArgumentException("You cannot have read more than the total number of pages!");
+            }
+            else cPages = input;
+        }
+        get
+        {
+            return cPages.ToString();
+        }
+
+    }
+
+    public Bookmarker(string titleP, string tPagesP, string cPagesP)
+    {
+        Title = titleP;
+        TPages = tPagesP;
+        CPages = cPagesP;
+    }
+
+    public override string ToString()
+    {
+        return $"You have read {((double)cPages / tPages):P} of \"{Title}\".\nYou have {(1 - ((double)cPages / tPages)):P} to go!";
+    }
+    public void Read(string pReadP)
+    {
+        int value = int.Parse(pReadP);
+        CPages += pReadP;
+    }
+}
---
+++ source/code/projects/Bookmarker/Bookmarker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+        string title, tPages, cPages;
+        try
+        {
+            Console.WriteLine("Enter the title of the book.");
+            title = Console.ReadLine();
+            Console.WriteLine("Enter the total number of pages.");
+            tPages = Console.ReadLine();
+            Console.WriteLine("Enter the page you stopped your reading at.");
+            cPages = Console.ReadLine();
+            Bookmarker book1 = new Bookmarker(title, tPages, cPages);
+            Console.WriteLine(book1);
+            Console.WriteLine("How many pages did you read?");
+            book1.Read(Console.ReadLine());
+            Console.WriteLine(book1);
+        }
+        catch (Exception ex)
+        { Console.WriteLine(ex.Message); }
+
+  }
+}
