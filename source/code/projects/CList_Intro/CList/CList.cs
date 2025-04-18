521953050dc9c23ad7c0e03f8b2d1d38c7c98cea
caubert <caubert@augusta.edu>
2025-04-18 11:23:53 -0400

    Adding simpler code for CLists.

---
+++ source/code/projects/CList_Intro/CList/CList.cs
@@ -0,0 +1,35 @@
+﻿public class CList<T>
+{
+  // A CList is … a Cell.
+  private Cell first;
+
+  // By default, a CList contains only an empty cell.
+  public CList()
+  {
+    first = null;
+  }
+
+  // A Cell is itself two things:
+  // - An element of data (of type T),
+  // - Another cell, containing the next element of data.
+  // We implement this using automatic properties:
+  private class Cell
+  {
+    public T Data { get; set; }
+    public Cell Next { get; set; }
+
+    public Cell(T dataP, Cell nextP)
+    {
+      Data = dataP;
+      Next = nextP;
+    }
+  }
+
+  // A method to add a cell at the beginning
+  // of the CList (to the left).
+  // We call it AddF for "Add First".
+  public void AddF(T dataP)
+  {
+    first = new Cell(dataP, first);
+  }
+}
---
+++ source/code/projects/CList_Intro/CList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    /* Example. */
+    CList<int> myList1 = new CList<int>();
+    myList1.AddF(1);
+    myList1.AddF(5);
+    myList1.AddF(2);
+    myList1.AddF(2);
+    myList1.AddF(1);
+    myList1.AddF(1);
+    myList1.AddF(3);
+    myList1.AddF(4);
+    myList1.AddF(5);
+    myList1.AddF(5);
+    myList1.AddF(5);
+    myList1.AddF(2);
+    myList1.AddF(2);
+  }
+}
