eed725b349e661fc98230666909483c9678085f6
caubert <caubert@augusta.edu>
2025-08-14 12:48:28 -0400

    Working on complexity notes.

---
+++ source/code/projects/GrowthMagnitudes/GrowthMagnitudes/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    // We implement our own factorial function:
+    static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return checked(factorial(n - 1) * n);
+    }
+
+    static void Main()
+    {
+        // As a reminder, this is the "biggest" integer
+        // value C# can handle:
+        Console.WriteLine(int.MaxValue);
+        // 2,147,483,647
+
+        // Remember that it's easy to trick C# into 
+        // overflowing:
+        int x = int.MaxValue;
+        int y = x + 1;
+        Console.WriteLine(y);
+
+        // Hence, we use the "checked" keyword
+        // to force C# to check that no overflow
+        // occurs:
+        try
+        {
+            y = checked(x + 1);
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("An overflow exception was thrown.");
+        }
+
+        // Now we illustrate various growth
+        // magnitude. Each "function" will have its
+        // input grow linearly, and we will count
+        // which input size makes the function
+        // outputs something that is bigger than
+        // the "biggest" integer value.
+
+        // Factorial
+        int inputfact = 0;
+        int outputfact;
+        try
+        {
+            while (true)
+            {
+                inputfact++;
+                outputfact = checked(factorial(inputfact));
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Factorial: an overflow exception was thrown once input reached " + inputfact + ".");
+            // Factorial: an overflow exception was thrown once input reached 13.
+            // 13! = 6,227,020,800
+        }
+
+        // Exponential
+        int inputexp = 0;
+        int outputexp;
+        try
+        {
+            while (true)
+            {
+                inputexp++;
+                checked
+                {
+                    outputexp = (int)Math.Pow(2, inputexp);
+                }
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Exponential: an overflow exception was thrown once input reached " + inputexp + ".");
+            // Exponential: an overflow exception was thrown once input reached 31.
+            // 2^31 = 2,147,483,648
+        }
+
+        // Cubic
+        int inputcubic = 0;
+        int outputcubic;
+        try
+        {
+            while (true)
+            {
+                inputcubic++;
+                checked
+                {
+                    outputcubic = (int)Math.Pow(inputcubic, 3);
+                }
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Cubic: an overflow exception was thrown once input reached " + inputcubic + ".");
+            // Cubic: an overflow exception was thrown once input reached 1291.
+            // 1,291^3 = 2,151,685,171
+        }
+
+        // Quadratic
+        int inputquad = 0;
+        int outputquad;
+        try
+        {
+            while (true)
+            {
+                inputquad++;
+                checked
+                {
+                    outputquad = (int)Math.Pow(inputquad, 2);
+                }
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Quadratic: an overflow exception was thrown once input reached " + inputquad + ".");
+            // Quadratic: an overflow exception was thrown once input reached 46341.
+            // 46,341^2 = 2,147,488,281
+        }
+
+        // Linearithmic
+        int inputlinearith = 81703000; // We do not start at 0 to save some time
+        int outputlinearith;
+        try
+        {
+            while (true)
+            {
+                inputlinearith++;
+                checked
+                {
+                    outputlinearith = (int)(inputlinearith * Math.Log(inputlinearith, 2));
+                }
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Linearithmic: an overflow exception was thrown once input reached " + inputlinearith + ".");
+            // Linearithmic: an overflow exception was thrown once input reached 81703409.
+            // 81,703,409 * (log(81,703,409)) = 81,703,409 * 26.2838929389692 = 2,147,483,654.90481
+        }
+
+        // Linear
+        int inputlinear = 2147483640;  // We do not start at 0 to save some time
+        int outputlinear;
+        try
+        {
+            while (true)
+            {
+                checked
+                {
+                    inputlinear++;
+                    outputlinear = inputlinear;
+                }
+            }
+        }
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Linear: an overflow exception was thrown once input reached " + inputlinear + ".");
+            // Linear: an overflow exception was thrown once input reached 2147483647.
+            // 2,147,483,647 is Max.IntValue
+        }
+
+        // Logarithmic
+        double inputlog = Double.MaxValue - 1;
+        // This is 1.7976931348623157E+308;
+        double outputlog= Math.Log(inputlog, 2);
+        Console.WriteLine("Logarithmic: log of " + inputlog + " is " + outputlog + ".");
+        // Logarithmic: log of 1.79769313486232E+308 is 1024.
+    }
+}
--- source/docs/programming_and_computer_usage/complexity.md
+++ source/docs/programming_and_computer_usage/complexity.md
@@ -1,22 +1,94 @@
-#  Some Notes on Complexity
+# Complexity
+
+## What makes a program "better"?
+
+There are multiple ways of measuring if a program is "better" than an other, assuming they perform similar functions:
+
+- Correctness: a program not producing the correct output is worst than a program that is always correct,
+- Security: a program leaking personal information or infesting your computer will always be worst than one that does not,
+- Memory: the less a program requires memory to be installed and to run efficiently, the better.
+- Time: the faster the program produces the output, the better.
+- Other measures: ergonomics, cost, compatibility with operating systems, licence, network efficiency, may also come into play to decide which program is "best".
+
+Some criteria may be subjective (usability, for example), but some can be approach objectively: in a first approximation, memory (also called "space") and time are the two preferred measures.
+
+## Orders of magnitude in growth rates
+
+### Principles
+
+When measuring space or time, it is important to take the size of the input into account: that a word-processing software can open a document containing 1 word in 0.0001 second is no good if it takes hours to open a 10 pages document.
+
+Hence, algorithms and programs are classified according to their *growth rates*, how "fast" their run time or space requirements grow when the input size grows.
+This growth rate is classified according to ["orders"](https://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions), using the [*Big O notation*](https://en.wikipedia.org/wiki/Big_O_notation), whose definition follows:
+
+Using $|\cdot|$ to denote the absolute value, we write:
+
+$$f(x) = O(g(x))$$
+
+if there exists positive numbers $n$ and $x_0$ such that
+
+$$|f(x)| \leqslant M |g(x)|$$
+
+for all $x \geqslant x_0$.
+
+### Common orders of magnitude
+
+Using $c>1$ for "a constant", $n$ for the size of the input and $\log$ for logarithm in base $2$, we have the following common [orders of magnitude](https://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions):
+
+- constant ($O(c)$),
+- logarithmic ($O(\log n)$),
+- linear ($O(n)$),
+- [linearithmic](https://en.wikipedia.org/wiki/Time_complexity#Quasilinear_time) ($O(n \times \log(n)))$),
+- polynomial ($O(n^c)$), which includes
+    - quadratic ($O(n^2)$),
+    - cubic ($O(n^3)$),
+- exponential ($O(c^n)$),
+- factorial ($O(n!)$).
+
+### Simplifications
+
+In typical usage the $O$ notation is *asymptotical* (we are interested in very large values), which means that only the contribution of the terms that grow "most quickly" is relevant. Hence, we can use the following rules:
+
+- If $f(x)$ is a sum of several terms, if there is one with largest growth rate, it can be kept, and all others omitted.
+- If $f(x)$ is a product of several factors, any constants (factors in the product that do not depend on $x$) can be omitted.
+
+There are additionaly some very useful [properties of the big O notation](https://www.geeksforgeeks.org/dsa/properties-of-asymptotic-notations/):
+
+- Reflexivity: $f(n) = O(f(n))$,
+- Transitivity: $f(n) = O(g(n))$ and $g(n) = O(h(n))$ implies $f(n) = O(h(n))$,
+- Constant factor: $f(n) = O(g(n))$ and $c > 1$ implies $c\times f(n) = O(g(n))$,
+- Sum rule: $f(n) = O(g(n))$ and $h(n) = O(k(n))$ implies $f(n) + h(n) = O(\max (g(n), k(n))$,
+- Product rule: $f(n) = O(g(n))$ and $h(n) = O(k(n))$ implies $f(n) \times h(n) = O(g(n) \times k(n))$,
+- Composition rule: $f(n) = O(g(n))$ and $g(n) = O(h(n))$ implies $f(g(n)) = O(h(n))$.
+
+$$
+\begin{align*}
+f(n) & = O(f(n)) \tag{Reflexivity}\\
+f(n) & = O(f(n)) \text{Reflexivity}
+\end{align*}
+$$
+
+<!--
+    Example 1:  f(n) = 3n2 + 2n + 1000Logn +  5000
+    After ignoring lower order terms, we get the highest order term as 3n2
+    After ignoring the constant 3, we get n2
+    Therefore the Big O value of this expression is O(n2)
+
+    Example 2 :  f(n) = 3n3 + 2n2 + 5n + 1
+    Dominant Term: 3n3
+    Order of Growth: Cubic (n3)
+    Big O Notation: O(n3)
+--> 
+
+
 
 Have a look at the [Big-O complexity chart](https://www.bigocheatsheet.com/):
 
 ![Big-O Complexity Chart](https://www.bigocheatsheet.com/img/big-o-complexity-chart.png)
 
-A function [has an order](https://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions), it can be for example
-
-- constant (O(c)),
-- logarithmic (O(log n)),
-- linear (O(n)),
-- [linearithmic](https://en.wikipedia.org/wiki/Time_complexity#Quasilinear_time) (O(n log n)),
-- quadratic (O(n^2)),
-- cubic (O(n^3)),
-- exponential (O(c^n)),
-- factorial (O(n!)).
 
 This can make a *very* significant difference, as exemplified in the following code:
 
 ```
-!include code/snippets/complexity.cs
+!include code/projects/GrowthMagnitudes/GrowthMagnitudes/Program.cs
 ```
