23a3795f94aed06864b3129a9d6023968de729af
Clément <Clment@domicile>
2025-09-18 13:23:52 -0400

    Tidy up source code.

--- source/code/projects/AList/AList/AList.cs
+++ source/code/projects/AList/AList/AList.cs
@@ -4,195 +4,191 @@ using System.Collections.Generic;
 
 public class AList<T> : ICollection<T>
 {
-    // Our implementation of list is so that
-    // the alist array is always of the "right
-    // size", so we don't need to keep track
-    // of a separate counter for the size of the
-    // list. This is more memory-
-    // intensive (because arrays get created
-    // frequently), but lighter to implement.
-    private T[] alist;
+  // Our implementation of list is so that
+  // the alist array is always of the "right
+  // size", so we don't need to keep track
+  // of a separate counter for the size of the
+  // list. This is more memory-
+  // intensive (because arrays get created
+  // frequently), but lighter to implement.
+  private T[] alist;
 
-    public AList()
+  public AList()
+  {
+    alist = new T[0];
+  }
+
+  public bool IsEmpty()
+  {
+    return alist.Length == 0;
+  }
+
+  public void Clear()
+  {
+    alist = new T[0];
+  }
+
+  // Attribute and property for the
+  // isReadOnly required by ICollection.
+  public bool isReadonly = false;
+  public bool IsReadOnly
+  {
+    get { return isReadonly; }
+    set { isReadonly = value; }
+  }
+
+  // Add is adding "to the left".
+  public void Add(T value)
+  {
+    if (isReadonly)
     {
-        alist = new T[0];
+      throw new InvalidOperationException(
+        "List is read-only, you cannot add to it."
+      );
     }
-
-    public bool IsEmpty()
+    else
     {
-        return alist.Length == 0;
+      // We create a larger array
+      // using the length of the existing
+      // array.
+      T[] newlist = new T[alist.Length + 1];
+      // We insert the new value "to the left".
+      newlist[0] = value;
+      // We copy the remaining values,
+      // shifting them by one.
+      for (int i = 0; i < alist.Length; i++)
+        newlist[i + 1] = alist[i];
+      // Finally, our new array becomes our
+      // alist attribute.
+      alist = newlist;
     }
+  }
 
-    public void Clear()
+  // Remove the first node containing the value
+  // if it exists and returns true,
+  // returns false otherwise.
+  public bool Remove(T value)
+  {
+    if (isReadonly)
     {
-        alist = new T[0];
+      throw new InvalidOperationException(
+        "List is read-only, you cannot remove from it."
+      );
     }
-
-    // Attribute and property for the 
-    // isReadOnly required by ICollection.
-    public bool isReadonly = false;
-    public bool IsReadOnly
+    bool removed = false;
+    if (!IsEmpty())
     {
-        get { return isReadonly; }
-        set { isReadonly = value; }
-    }
-
-    // Add is adding "to the left".
-    public void Add(T value)
-    {
-        if (isReadonly)
+      int i = 0;
+      while (i < alist.Length && !removed)
+      {
+        if (alist[i].Equals(value))
         {
-            throw new InvalidOperationException(
-              "List is read-only, you cannot add to it."
-            );
+          removed = true;
+
+          // We create the array that will replace
+          // the current one.
+
+          T[] newList = new T[alist.Length - 1];
+          // We first copy the values
+          // before the value we found:
+          for (int j = 0; j < i; j++)
+            newList[j] = alist[j];
+          // We now copy the values *after*
+          // the value we found.
+          for (int j = i; j < newList.Length; j++)
+            newList[j] = alist[j + 1];
+          alist = newList;
         }
-        else
-        {
-            // We create a larger array
-            // using the length of the existing
-            // array.
-            T[] newlist = new T[alist.Length + 1];
-            // We insert the new value "to the left".
-            newlist[0] = value;
-            // We copy the remaining values,
-            // shifting them by one.
-            for (int i = 0; i < alist.Length; i++)
-                newlist[i + 1] = alist[i];
-            // Finally, our new array becomes our
-            // alist attribute.
-            alist = newlist;
-        }
+        i++;
+      }
     }
+    return removed;
+  }
 
-
-    // Remove the first node containing the value
-    // if it exists and returns true,
-    // returns false otherwise.
-    public bool Remove(T value)
+  public bool Contains(T value)
+  {
+    // We start by assuming we did not find
+    // the target value.
+    bool found = false;
+    // We go through the array.
+    for (int i = 0; i < alist.Length; i++)
     {
-        if (isReadonly)
-        {
-            throw new InvalidOperationException(
-              "List is read-only, you cannot remove from it."
-            );
-        }
-        bool removed = false;
-        if (!IsEmpty())
-        {
-            int i = 0;
-            while (i < alist.Length && !removed)
-            {
-                if (alist[i].Equals(value))
-                {
-                    removed = true;
-
-                    // We create the array that will replace
-                    // the current one.
-
-                    T[] newList = new T[alist.Length - 1];
-                    // We first copy the values
-                    // before the value we found:
-                    for (int j = 0; j < i; j++)
-                        newList[j] = alist[j];
-                    // We now copy the values *after*
-                    // the value we found.
-                    for (int j = i; j < newList.Length; j++)
-                        newList[j] = alist[j + 1];
-                    alist = newList;
-                }
-                i++;
-            }
-        }
-        return removed;
+      if (alist[i].Equals(value))
+        found = true;
     }
+    return found;
+  }
 
-    public bool Contains(T value)
+  // Adding "to the right" (as the last value).
+  public void AddR(T value)
+  {
+    if (isReadonly)
     {
-        // We start by assuming we did not find
-        // the target value.
-        bool found = false;
-        // We go through the array.
-        for (int i = 0; i < alist.Length; i++)
-        {
-            if (alist[i].Equals(value)) found = true;
-        }
-        return found;
+      throw new InvalidOperationException(
+        "List is read-only, you cannot add to it."
+      );
     }
-
-
-    // Adding "to the right" (as the last value).
-    public void AddR(T value)
+    else
     {
-        if (isReadonly)
-        {
-            throw new InvalidOperationException(
-              "List is read-only, you cannot add to it."
-            );
-        }
-        else
-        {
-            // We resize the array using 
-            // built-in method.
-            Array.Resize(ref alist, alist.Length + 1);
-            // We insert the new value "at the end".
-            alist[alist.Length - 1] = value;
-        }
+      // We resize the array using
+      // built-in method.
+      Array.Resize(ref alist, alist.Length + 1);
+      // We insert the new value "at the end".
+      alist[alist.Length - 1] = value;
     }
+  }
 
-    // Count property simply returns
-    // the length of the alist array
-    // if it is not null.
-    public int Count
+  // Count property simply returns
+  // the length of the alist array
+  // if it is not null.
+  public int Count
+  {
+    get { return alist.Length; }
+  }
+
+  // Copies the elements of the ICollection to an Array, starting at a particular Array index.
+  public void CopyTo(T[] array, int arrayIndex)
+  {
+    if (array == null)
+      // The array into which we copy
+      // cannot be null.
+      throw new ArgumentNullException();
+    if (arrayIndex < 0)
+      // The index must be strictly positive.
+      throw new ArgumentOutOfRangeException();
+    if (Count > array.Length - arrayIndex)
+      // If there is not enough room in the
+      // array given as argument to copy
+      // our list, we throw an exception.
+      throw new ArgumentException(
+        $"The destination array does not have enough space ({array.Length}) to copy the list ({Count}) starting at the given index ({arrayIndex})."
+      );
+    // If none of the exceptions were triggered,
+    // we can copy the values at the right index.
+    for (int i = 0; i < alist.Length; i++)
     {
-        get
-        {
-            return alist.Length;
-        }
+      array[i + arrayIndex] = alist[i];
     }
+  }
 
-    // Copies the elements of the ICollection to an Array, starting at a particular Array index.
-    public void CopyTo(T[] array, int arrayIndex)
+  /* The following is "bureaucracy" to realize the
+   * ICollection interface. */
+
+  public IEnumerator<T> GetEnumerator()
+  {
+    if (!IsEmpty())
     {
-        if (array == null)
-            // The array into which we copy
-            // cannot be null.
-            throw new ArgumentNullException();
-        if (arrayIndex < 0)
-            // The index must be strictly positive.
-            throw new ArgumentOutOfRangeException();
-        if (Count > array.Length - arrayIndex)
-            // If there is not enough room in the 
-            // array given as argument to copy
-            // our list, we throw an exception.
-            throw new ArgumentException(
-              $"The destination array does not have enough space ({array.Length}) to copy the list ({Count}) starting at the given index ({arrayIndex})."
-            );
-        // If none of the exceptions were triggered, 
-        // we can copy the values at the right index.
-        for (int i = 0; i < alist.Length; i++)
-        {
-            array[i + arrayIndex] = alist[i];
-        }
+      for (int i = 0; i < alist.Length; i++)
+      {
+        yield return alist[i];
+      }
     }
+  }
 
-    /* The following is "bureaucracy" to realize the 
-     * ICollection interface. */
+  IEnumerator IEnumerable.GetEnumerator()
+  {
+    return this.GetEnumerator(); // call the generic version of the method
+  }
 
-    public IEnumerator<T> GetEnumerator()
-    {
-        if (!IsEmpty())
-        {
-            for (int i = 0; i < alist.Length; i++)
-            {
-                yield return alist[i];
-            }
-        }
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return this.GetEnumerator(); // call the generic version of the method
-    }
-
-    /* We are done realizing the ICollection interface. */
+  /* We are done realizing the ICollection interface. */
 }
--- source/code/projects/AList/AList/Program.cs
+++ source/code/projects/AList/AList/Program.cs
@@ -6,26 +6,33 @@ class Program
   {
     AList<int> myList1 = new AList<int>();
 
-        /* Testing IsEmpty(), Clear() and Count. */
-        Console.WriteLine("The list is empty: " + myList1.IsEmpty()); // Should display "True".
-        Console.WriteLine("List size: " + myList1.Count); // Should display "0".
-        myList1.Add(10);
-        Console.WriteLine("The list is empty: " + myList1.IsEmpty()); // Should display "False".
-        Console.WriteLine("List size: " + myList1.Count); // Should display "1".
-        myList1.Clear();
-        Console.WriteLine("The list is empty: " + myList1.IsEmpty()); // Should display "True".
-        Console.WriteLine("List size: " + myList1.Count); // Should display "0".
-        myList1.Add(10);
-        myList1.Remove(10);
-        Console.WriteLine("The list is empty: " + myList1.IsEmpty()); // Should display "True".
-        Console.WriteLine("List size: " + myList1.Count); // Should display "0".
+    /* Testing IsEmpty(), Clear() and Count. */
+    Console.WriteLine(
+      "The list is empty: " + myList1.IsEmpty()
+    ); // Should display "True".
+    Console.WriteLine("List size: " + myList1.Count); // Should display "0".
+    myList1.Add(10);
+    Console.WriteLine(
+      "The list is empty: " + myList1.IsEmpty()
+    ); // Should display "False".
+    Console.WriteLine("List size: " + myList1.Count); // Should display "1".
+    myList1.Clear();
+    Console.WriteLine(
+      "The list is empty: " + myList1.IsEmpty()
+    ); // Should display "True".
+    Console.WriteLine("List size: " + myList1.Count); // Should display "0".
+    myList1.Add(10);
+    myList1.Remove(10);
+    Console.WriteLine(
+      "The list is empty: " + myList1.IsEmpty()
+    ); // Should display "True".
+    Console.WriteLine("List size: " + myList1.Count); // Should display "0".
 
-
-        /* Adding more elements, and then displaying the list.*/
-        for (int i = 0; i < 15; i++)
-        {
-            myList1.Add(i);
-        }
+    /* Adding more elements, and then displaying the list.*/
+    for (int i = 0; i < 15; i++)
+    {
+      myList1.Add(i);
+    }
 
     // A benefit of realizing the ICollection interface
     // is that we can iterate over elements of lists now:
@@ -34,39 +41,38 @@ class Program
       Console.Write(item + " ");
     }
     Console.WriteLine("");
-        Console.WriteLine("List size: " + myList1.Count); // Should display "15".
+    Console.WriteLine("List size: " + myList1.Count); // Should display "15".
 
-        /* Adding "from the rigt" */
-        for (int i = -10; i < 0; i++)
-        {
-            myList1.AddR(i);
-        }
-        foreach (var item in myList1)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("");
-        Console.WriteLine("List size: " + myList1.Count); // Should display "15".
+    /* Adding "from the rigt" */
+    for (int i = -10; i < 0; i++)
+    {
+      myList1.AddR(i);
+    }
+    foreach (var item in myList1)
+    {
+      Console.Write(item + " ");
+    }
+    Console.WriteLine("");
+    Console.WriteLine("List size: " + myList1.Count); // Should display "15".
 
+    /* Testing Remove in more detail.*/
 
-        /* Testing Remove in more detail.*/
+    // 1. Removing a value that is present one time.
+    Console.Write(
+      "We removed an occurence of 10: "
+        + myList1.Remove(10)
+        + ".\n"
+    );
+    foreach (var item in myList1)
+    {
+      Console.Write(item + " ");
+    }
+    Console.WriteLine("");
 
-        // 1. Removing a value that is present one time.
-        Console.Write(
-"We removed an occurence of 10: "
-+ myList1.Remove(10)
-+ ".\n"
-);
-        foreach (var item in myList1)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("");
-
-        // 2. Removing a value that is present three times.
-        myList1.Add(1);
-        myList1.Add(1);
-        Console.Write(
+    // 2. Removing a value that is present three times.
+    myList1.Add(1);
+    myList1.Add(1);
+    Console.Write(
       "We removed an occurence of 1: "
         + myList1.Remove(1)
         + ".\n"
@@ -77,7 +83,7 @@ class Program
     }
     Console.WriteLine("");
 
-        // 3. Removing a value that is not in the list. 
+    // 3. Removing a value that is not in the list.
     Console.Write(
       "We removed an occurence of 20: "
         + myList1.Remove(20)
@@ -89,11 +95,10 @@ class Program
     }
     Console.WriteLine("");
 
-
-        /* 
-         * Advanced testing: 
-         * IsReadOnly and CopyTo.
-         */
+    /*
+     * Advanced testing:
+     * IsReadOnly and CopyTo.
+     */
     Console.WriteLine(
       "Our list is read-only: " + myList1.IsReadOnly + "."
     );
@@ -127,9 +132,9 @@ class Program
     {
       Console.WriteLine(ex.Message);
     }
-        // We insert a dummy value 
-        // into the array that will receive
-        // a copy of the list at index 1.
+    // We insert a dummy value
+    // into the array that will receive
+    // a copy of the list at index 1.
     array[0] = -10;
     myList1.CopyTo(array, 1);
     Console.WriteLine("The array contains:");
